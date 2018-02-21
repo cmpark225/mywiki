337c0e912f0b5af0a360a1483b31c56df1555278
sally225 <cmpark225@gmail.com>
2018-02-21 16:35:30 +0900

    Update serialize a model instance.py

--- django/serialize a model instance.py
+++ django/serialize a model instance.py
@@ -1,5 +1,6 @@
 >>> Q.objects.filter(id=6).values()
 [{'short': u'oui', 'deleted': False, 'language_id': 3L, 'long': u'oui', 'place_holder': u'oui', 'id': 6L, 'question_id': 8L}]
+>>> from django.forms.models import model_to_dict
 >>> model_to_dict(Q.objects.filter(id=6)[0])
 {'short': u'oui', 'language': 3L, 'deleted': False, 'question': 8L, 'long': u'oui', 'place_holder': u'oui'}
 
